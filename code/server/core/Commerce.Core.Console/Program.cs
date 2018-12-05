0106f3b778696639aa484c48cad685d919a636eb
Ihar Zharykau <ihar.zharykau@fortegrp.net>
2018-12-05 20:01:31 +0300

    Initial commit

---
+++ code/server/core/Commerce.Core.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Commerce.Core.Tools;
+
+namespace Commerce.Core.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(CommonTools.GetSomeStr());
+        }
+    }
+}
---
+++ code/server/core/Commerce.Core/Tools/CommonTools.cs
@@ -0,0 +1,10 @@
+namespace Commerce.Core.Tools
+{
+    public static class CommonTools
+    {
+        public static string GetSomeStr()
+        {
+            return "Hello, dotnet!";
+        }
+    }
+}
